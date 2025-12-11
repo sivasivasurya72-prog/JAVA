cf4272cf2989586b08c0dc2676f36d8ad053477e
SIVA SURYA S <sivasivasurya72@gmail.com>
2025-12-11 15:23:27 +0530

    Multiline

--- multiline.java
+++ multiline.java
@@ -1,7 +1,9 @@
-class Main {
+import java.util.Scanner;
+public class Multiline {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         string line
         System.out.println("Line 1 \n Line 2 \n Line 3");
     }
 }
+
