2c7333ef2e91b38ced5a23a22645aea6f8eb5372
SIVA SURYA S <sivasivasurya72@gmail.com>
2025-12-11 12:13:35 +0530

    Add files via upload

---
+++ Two_number_in_Two_line.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Two_number_in_Two_line {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
