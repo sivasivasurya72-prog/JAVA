65fc3769b7a03931bb645f12653e81ca02418a05
SIVA SURYA S <sivasivasurya72@gmail.com>
2025-12-11 15:20:53 +0530

    Add files via upload

---
+++ bloodgroup.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class bloodgroup {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String bg = sc.next();
+        System.out.println("Blood Group: " + bg);
+   
+    sc.close();
+}
+}
