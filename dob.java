9e97be0badc65d91211d52230033d732bfa9f7c9
SIVA SURYA S <sivasivasurya72@gmail.com>
2025-12-11 15:10:59 +0530

    Add files via upload

---
+++ dob.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int d = sc.nextInt();
+        int m = sc.nextInt();
+        int y = sc.nextInt();
+        System.out.println(d + "/" + m + "/" + y);
+    sc.close();
+    }
+}
