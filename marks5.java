9cbb3fff5a9a512444266a558ca814cbbba15829
SIVA SURYA S <sivasivasurya72@gmail.com>
2025-12-11 15:20:24 +0530

    Marks5

--- marks5.java
+++ marks5.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class Main {
+public class Marks5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         for(int i=0; i<5; i++) {
@@ -9,3 +9,4 @@ class Main {
     sc.close();
     }
 }
+
