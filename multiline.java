37c2b3dc5fd93828707300e6cc10d3df9c48d097
SIVA SURYA S <sivasivasurya72@gmail.com>
2025-12-11 15:22:25 +0530

    Add files via upload

---
+++ multiline.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        string line
+        System.out.println("Line 1 \n Line 2 \n Line 3");
+    }
+}
