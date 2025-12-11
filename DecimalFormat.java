b7ca56197b7a0f4a75a503677edee772aa8990cc
SIVA SURYA S <sivasivasurya72@gmail.com>
2025-12-11 11:33:08 +0530

    Add files via upload

---
+++ DecimalFormat.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class DecimalFormat {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float f = sc.nextFloat();
+        System.out.printf("%.2f", f);
+    }
+}
