0a54175433a8deb06e2a0d01e41fda5c7b581b5e
SIVA SURYA S <sivasivasurya72@gmail.com>
2025-12-11 15:19:34 +0530

    Add files via upload

---
+++ marks5.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for(int i=0; i<5; i++) {
+            int m = sc.nextInt();
+            System.out.println(m);
+        }
+    sc.close();
+    }
+}
